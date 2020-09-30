45658ebcb895444e791653246dc6af7f858e3681
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2020-09-30 16:59:12 +0800

    Update usage to be clearer that that it is the payload that is passed

--- usage.py
+++ usage.py
@@ -1,14 +1,15 @@
 from connection import Socket
 
-def callback1(msg):
-    print("Callback 1: ", msg)
+def callback1(payload):
+    print("Callback 1: ", payload)
 
-def callback2(msg):
-    print("Callback 2: ", msg)
+def callback2(payload):
+    print("Callback 2: ", payload)
 
 URL = "ws://localhost:4000/socket/websocket"
 s = Socket(URL)
 s.connect()
+
 channel_1 = s.set_channel("realtime:public:todos")
 channel_1.join().on("UPDATE", callback1)
 
@@ -16,4 +17,5 @@ channel_2 = s.set_channel("realtime:public:users")
 channel_2.join().on("*", callback2)
 
 if __name__ == "__main__":
+
     s.listen()
