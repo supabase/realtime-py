0a60297c9cce91b316355fa95eb4f25031b08a24
rizkyrajitha <rajithagunathilake@gmail.com>
2021-10-15 12:23:20 +0530

    added gh actions for ci

---
+++ tests/type_test.py
@@ -0,0 +1,16 @@
+import os
+from realtime_py.connection import Socket
+
+def getSock():
+    SUPABASE_ID = os.getenv("SUPABASE_ID") 
+    API_KEY = os.getenv("API_KEY") 
+
+    URL = f"wss://{SUPABASE_ID}.supabase.co/realtime/v1/websocket?apikey={API_KEY}&vsn=1.0.0"
+    s = Socket(URL)
+    s.connect()
+
+    return s
+
+def test():
+    sock = getSock()
+    assert isinstance(sock , Socket) == True
