a810aa37ab040f1f63cf88c8ea206ba4a3b1818d
Joel Lee <joel@joellee.org>
2021-10-27 21:12:42 -0500

    chore: update versions

--- realtime_py/__init__.py
+++ realtime_py/__init__.py
@@ -1,5 +1,8 @@
+__version__ = "0.1.3"
+
 from realtime_py.channel import CallbackListener, Channel
 from realtime_py.connection import Socket
 from realtime_py.exceptions import NotConnectedError
 from realtime_py.message import *
 from realtime_py.transformers import *
+
