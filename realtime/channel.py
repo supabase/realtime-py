e5bd3330cd06bb8ca922279cfc8948a4c4b94cf5
Jean-Louis Huynen <huynenjl@gmail.com>
2023-10-04 12:44:39 +0200

    add: [send] sending to the channel poc

--- realtime/channel.py
+++ realtime/channel.py
@@ -53,10 +53,37 @@ class Channel:
         :return: None
         """
         if self.socket.version == 1:
-            join_req = dict(topic=self.topic, event="phx_join",
+            join_req = dict(topic=self.topic, event=ChannelEvents.join,
                         payload={}, ref=None)
         elif self.socket.version == 2:
             join_req = [None, None, self.topic, ChannelEvents.join, {}]
+
+        try:
+            await self.socket.ws_connection.send(json.dumps(join_req))
+        except Exception as e:
+            print(str(e))  # TODO: better error propagation
+            return
+
+    def leave(self) -> Channel:
+        """
+        Wrapper for async def _leave() to expose a non-async interface
+        Essentially gets the only event loop and attempt leaving a topic
+        :return: Channel
+        """
+        loop = asyncio.get_event_loop()  # TODO: replace with get_running_loop
+        loop.run_until_complete(self._join())
+        return self
+
+    async def _leave(self) -> None:
+        """
+        Coroutine that attempts to join Phoenix Realtime server via a certain topic
+        :return: None
+        """
+        if self.socket.version == 1:
+            join_req = dict(topic=self.topic, event=ChannelEvents.leave,
+                        payload={}, ref=None)
+        elif self.socket.version == 2:
+            join_req = [None, None, self.topic, ChannelEvents.leave, {}]
 
         try:
             await self.socket.ws_connection.send(json.dumps(join_req))
@@ -81,3 +108,35 @@ class Channel:
         """
         self.listeners = [
             callback for callback in self.listeners if callback.event != event]
+
+    def send(self, event_name: str, payload:str) -> None:
+        """
+        Wrapper for async def _send() to expose a non-async interface
+        Essentially gets the only event loop and attempt sending a payload
+        to a topic
+        :param event_name: The event_name: it must match the first argument of a handle_in function on the server channel module.
+        :param payload: The payload to be sent to the phoenix server
+        :return: None
+        """
+        loop = asyncio.get_event_loop()  # TODO: replace with get_running_loop
+        loop.run_until_complete(self._send(event_name, payload))
+        return self
+
+    async def _send(self, event_name: str, payload:str) -> None:
+        """
+        Coroutine that attempts to join Phoenix Realtime server via a certain topic
+        :param event_name: The event_name: it must match the first argument of a handle_in function on the server channel module.
+        :param payload: The payload to be sent to the phoenix server
+        :return: None
+        """
+        if self.socket.version == 1:
+            msg = dict(topic=self.topic, event=event_name,
+                        payload=payload, ref=None)
+        elif self.socket.version == 2:
+            msg = [3, 3, self.topic, event_name, payload]
+
+        try:
+            await self.socket.ws_connection.send(json.dumps(msg))
+        except Exception as e:
+            print(str(e))  # TODO: better error propagation
+            return
