4a13541a0d0442e4845ce2e46c809701010928f8
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2024-04-03 10:29:28 -0300

    Update app.py

--- example/app.py
+++ example/app.py
@@ -1,31 +1,35 @@
 from realtime.connection import Socket
-
+from realtime.channel import CallbackListener, Channel
+import os
+import time
 
 def broadcast_callback(payload):
     print("broadcast: ", payload)
+
+
 def presence_callback(payload):
     print("presence: ", payload)
+
+
 def postgres_changes_callback(payload):
     print("postgres_changes: ", payload)
 
+
+async def realtime(payload):
+    print("async realtime ", payload)
+
 if __name__ == "__main__":
-    URL = "wss://<project>.supabase.co"
-    JWT = "<token>"
-    s = Socket(URL, JWT)
-    s.connect()
-
-    channel = s.set_channel("broadcast-test",
-                            {
-                                "broadcast": { "ack": True, "self": True },
-                                "postgres_changes": [
-                                    {
-                                    "event": "*",
-                                    "schema": "public",
-                                    "table": "*"
-                                    }
-                                    ]})
-    channel.join().on("broadcast", dict(event ="test"), broadcast_callback)
-    channel.join().on("presence", dict(event ="sync"), presence_callback)
-    channel.join().on("postgres_changes", dict(event ="*"), postgres_changes_callback)
-
-    s.listen()
+    ID = os.getenv("SUPABASE_ID")
+    URL = f"https://{ID}.supabase.co"
+    JWT = os.getenv("API_KEY")
+    broad_socket = Socket(URL, JWT, auto_reconnect=True)
+    broadcast_channel = Channel(broad_socket, topic="broadcast-test")
+    broadcast_channel.on_broadcast("test",callback=broadcast_callback).subscribe()
+    time.sleep(6)
+    s = Socket(URL, JWT, auto_reconnect=True)
+    channel = Channel(s, topic="changes-test")
+    channel.on_postgres_changes(table="realtime_test", schema="public", event="*",callback=postgres_changes_callback).eq("id",10).subscribe()
+    ss = Socket(URL, JWT, auto_reconnect=True)
+    ss.connect()
+    socket_channel = ss.set_channel(channel)
+    socket_channel.join().on("broadcast-test", dict(event ="test"), broadcast_callback)
