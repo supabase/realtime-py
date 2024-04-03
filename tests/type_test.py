e784d3425c0ce50b94b4dcc10fde5fbe14a86108
Rodrigo Mansueli Nunes <rodrigo@mansueli.com>
2024-04-03 10:46:42 -0300

    Update type_test.py

--- tests/type_test.py
+++ tests/type_test.py
@@ -3,8 +3,8 @@ from realtime.connection import Socket
 
 
 def getSock():
-    SUPABASE_ID = 'kzwoknoxuuvryajohvij'#os.getenv("SUPABASE_ID")
-    API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6d29rbm94dXV2cnlham9odmlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwODE5MDE3OCwiZXhwIjoyMDIzNzY2MTc4fQ.5CmeXUIg9mFBGF7ZPdE4Td5LdeL_mIx9h5rV0ptyPfQ'#os.getenv("API_KEY")
+    SUPABASE_ID = os.getenv("SUPABASE_ID")
+    API_KEY = os.getenv("API_KEY")
     print(SUPABASE_ID, API_KEY)
     URL = f"wss://{SUPABASE_ID}.supabase.co"
     s = Socket(URL, API_KEY)
