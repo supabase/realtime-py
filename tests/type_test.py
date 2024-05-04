a9f6a27dc1fd62f1a951319327e2b61be2f58665
Juan Carlos <juancarlospaco@gmail.com>
2024-05-03 21:45:57 -0300

    Update .pre-commit-config.yaml

--- tests/type_test.py
+++ tests/type_test.py
@@ -1,9 +1,11 @@
 import os
+
 from realtime.connection import Socket
 
+
 def getSock():
-    SUPABASE_ID = os.getenv("SUPABASE_ID") 
-    API_KEY = os.getenv("API_KEY") 
+    SUPABASE_ID = os.getenv("SUPABASE_ID")
+    API_KEY = os.getenv("API_KEY")
 
     URL = f"wss://{SUPABASE_ID}.supabase.co/realtime/v1/websocket?apikey={API_KEY}&vsn=1.0.0"
     s = Socket(URL)
@@ -11,6 +13,7 @@ def getSock():
 
     return s
 
+
 def test():
     sock = getSock()
-    assert isinstance(sock , Socket)
+    assert isinstance(sock, Socket)
