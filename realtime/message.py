a8131009c710fa35f082761a3738440aefa6a2e6
Juan Carlos <juancarlospaco@gmail.com>
2024-05-03 21:46:13 -0300

    Update .pre-commit-config.yaml

--- realtime/message.py
+++ realtime/message.py
@@ -8,13 +8,16 @@ class Message:
     """
     Dataclass abstraction for message
     """
+
     event: str
     payload: Dict[str, Any]
     ref: Any
     topic: str
 
     def __hash__(self):
-        return hash((self.event, tuple(list(self.payload.values())), self.ref, self.topic))
+        return hash(
+            (self.event, tuple(list(self.payload.values())), self.ref, self.topic)
+        )
 
 
 class ChannelEvents(str, Enum):
