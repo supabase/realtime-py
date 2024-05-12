204f1f3c14103a80a1bd3e5ec4c266343a9b5d4e
Juan Carlos <juancarlospaco@gmail.com>
2024-05-12 18:14:37 -0300

    Fix #82

---
+++ tests/tests_close.py
@@ -0,0 +1,28 @@
+import os
+
+from realtime.connection import Socket
+
+
+def callback(payload):
+    print("Callback:\t", payload)
+
+
+def test():
+    SUPABASE_ID = os.environ.get("SUPABASE_ID")
+    API_KEY = os.environ.get("SUPABASE_KEY")
+
+    URL = f"wss://{SUPABASE_ID}.supabase.co/realtime/v1/websocket?apikey={API_KEY}&vsn=1.0.0"
+
+    s = Socket(URL)
+    s.connect()
+
+    channel_1 = s.set_channel("realtime:public:sample")
+    channel_1.join().on("INSERT", callback)
+
+    print(dir(s))
+    s.close()
+    assert s.ws_connection.closed, "Connection was not closed as expected."
+
+
+if __name__ == "__main__":
+    test()
