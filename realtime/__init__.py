386eddb7e2f6ccfbc3b084ff9fe5ed7b408623a9
Juan Carlos <juancarlospaco@gmail.com>
2024-08-08 16:59:45 -0300

    RealtimeClient added

--- realtime/__init__.py
+++ realtime/__init__.py
@@ -1,6 +1,7 @@
 __version__ = "2.0.0"
 
 from realtime.channel import CallbackListener, Channel
+from realtime.client import RealTimeClient
 from realtime.connection import Socket
 from realtime.exceptions import NotConnectedError
 from realtime.message import *
