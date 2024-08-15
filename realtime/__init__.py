50dcea278d411724a5493e81c471a9bb58e81547
Andrew Smith <a.smith@silentworks.co.uk>
2024-08-15 08:06:26 +0000

    ci: add release please to the workflow

--- realtime/__init__.py
+++ realtime/__init__.py
@@ -1,7 +1,6 @@
-__version__ = "2.0.0"
-
 from realtime.channel import CallbackListener, Channel
 from realtime.connection import Socket
 from realtime.exceptions import NotConnectedError
 from realtime.message import *
 from realtime.transformers import *
+from realtime.version import __version__
