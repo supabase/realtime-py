add88995d9571e843c42f585298dd851d7dc5869
Yorick van Zweeden <git@yorickvanzweeden.nl>
2024-09-20 14:23:06 +0200

    Update exports

--- realtime/__init__.py
+++ realtime/__init__.py
@@ -6,7 +6,8 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 from realtime._async.channel import AsyncRealtimeChannel
 from realtime._async.client import AsyncRealtimeClient
 from realtime._async.presence import AsyncRealtimePresence
-from realtime._sync.client import SyncRealtimeChannel, SyncRealtimeClient
+from realtime._sync.channel import SyncRealtimeChannel
+from realtime._sync.client import SyncRealtimeClient
 from realtime.exceptions import (
     AuthorizationError,
     ConnectionFailedError,
@@ -19,6 +20,8 @@ from realtime.types import (
     ChannelEvents,
     ChannelStates,
     Presence,
+    PresenceOnJoinCallback,
+    PresenceOnLeaveCallback,
     RealtimeChannelOptions,
     RealtimePostgresChangesListenEvent,
     RealtimePresenceEvents,
@@ -30,20 +33,22 @@ __all__ = [
     "AsyncRealtimeClient",
     "AsyncRealtimeChannel",
     "AsyncRealtimePresence",
+    "SyncRealtimeClient",
+    "SyncRealtimeChannel",
     "ChannelEvents",
     "ChannelStates",
     "RealtimeSubscribeStates",
     "RealtimePresenceEvents",
     "RealtimePostgresChangesListenEvent",
     "RealtimeChannelOptions",
+    "Presence",
+    "PresenceOnJoinCallback",
+    "PresenceOnLeaveCallback",
     "RealtimeError",
     "NotConnectedError",
     "AuthorizationError",
     "ConnectionFailedError",
     "ReconnectionFailedError",
     "InvalidMessageError",
-    "SyncRealtimeClient",
-    "SyncRealtimeChannel",
-    "Presence",
     "__version__",
 ]
