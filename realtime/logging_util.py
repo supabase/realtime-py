943dd2fcce8de75266be90f1adf1910a524716b9
Etienne Stalmans <etienne@supabase.io>
2024-10-01 12:30:55 +0200

    chore: adding a logger masking filter
    Use a logging.Filter to redact JWT tokens that may be in log messages.

---
+++ realtime/logging_util.py
@@ -0,0 +1,19 @@
+import logging
+import re
+
+redact = r"(eyJh\w*\.)(\w*)\."
+
+class TokenMaskingFilter(logging.Filter):
+    """Mask access_tokens in logs"""
+
+    def filter(self, record):
+        record.msg = self.sanitize_line(record.msg)
+        return True
+
+    @staticmethod
+    def sanitize_line(line):
+        def gred(g):
+            """Redact the payload of the JWT, keeping the header and signature"""
+            return f"{g.group(1)}REDACTED." if len(g.groups()) > 1 else g
+
+        return re.sub(redact, gred, line)
